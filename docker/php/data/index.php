7e00299a128eb5104744a7ab99e2b29136c2fef9
sz3jdii <swiatkowskiam@gmail.com>
2020-12-02 23:23:15 +0100

    Dodanie bazy sql

--- docker/php/data/index.php
+++ docker/php/data/index.php
@@ -1,13 +1,28 @@
 <?php 
 
-$servername = getenv('MYSQL_IP');
-$username = getenv('MYSQL_ROOT_USER');
-$password = getenv('MYSQL_ROOT_PASSWORD');
+$servername = 'db';
+$username = 'root';
+$password = 'toor';
  
 $conn = mysqli_connect($servername, $username, $password);
 if (!$conn) {
    exit('Nie polaczono: '.mysqli_connect_error().PHP_EOL);
 }
-echo 'Hello Adam!'.PHP_EOL;
+echo 'Hello Adam! Proxy dziala!'.PHP_EOL;
  
-echo 'Successful database connection!'.PHP_EOL;
+echo 'Polaczono do bazy!'.PHP_EOL;
+
+die();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Nie dziala :(</title>
+</head>
+<body>
+   Nie dziala proxy :(
+</body>
+</html>
